8ac721e60a6aa5bda8753cb021c8c6eeba79cc97
Jorge Luis <jorjoluiso@gmail.com>
2024-12-11 15:26:20 -0500

    [IMP] Categories and Products

--- web_portal/__manifest__.py
+++ web_portal/__manifest__.py
@@ -1,23 +1,14 @@
-# Copyright 2020 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
 {
-    'name': "My Web Portal",
-
-    'summary': "My Web Portal",
-
-    'author': "Jorge Luis",
-    'website': "https://mestizos.dev",
-
-    'category': 'website',
+    "name": "My Web Portal",
+    "summary": "My Web Portal",
+    "author": "Jorge Luis,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-ecuador",
+    "category": "website",
     "version": "18.0.1.0.0",
     "license": "AGPL-3",
-
-    'depends': ['portal'],
-
-    'data': [
+    "depends": ["portal"],
+    "data": [
         # 'security/ir.model.access.csv',
-        'views/home_web_portal.xml',
+        "views/home_web_portal.xml",
     ],
 }
-
--- web_portal/controllers/main.py
+++ web_portal/controllers/main.py
@@ -1,4 +1,5 @@
 from odoo.http import request, route
+
 from odoo.addons.website.controllers.main import Home
 
 
@@ -7,9 +8,39 @@ class Main(Home):
     def index(self, **kwargs):
         super().index(**kwargs)
 
+        Category = request.env["product.category"]
+        categories = Category.sudo().search([])
+
         res = request.render(
             "web_portal.home_web_portal",
-            {},
+            {"categories": categories},
         )
 
         return res
+
+    @route(
+        "/hola/odoo/v1/products/category/<int:category_id>", auth="public", website=True
+    )
+    def products_category(self, category_id, **kwargs):
+        result = (
+            request.env["product.category"]
+            .sudo()
+            .search_read([("id", "=", category_id)], ["name"], limit=1)
+        )
+
+        if result:
+            categorie_name = f"Products of {result[0]["name"]}"
+
+            Product = request.env["product.template"]
+            products = Product.sudo().search([("categ_id", "=", category_id)])
+        else:
+            categorie_name = "Products without category"
+            products = False
+
+        return request.render(
+            "web_portal.products_category_web_portal",
+            {
+                "products": products,
+                "categorie_name": categorie_name,
+            },
+        )
