b3df698c0423f8ea373fe192de680aa07b75cfe1
Jorge Luis <jorgequiguango@outlook.com>
2024-12-19 21:53:21 -0500

    [REF] Friend and Country

--- greet/models/friend.py
+++ greet/models/friend.py
@@ -1,3 +1,5 @@
+import re
+
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
@@ -7,17 +9,27 @@ class Friend(models.Model):
     _description = "Friend"
     _inherits = {"res.partner": "partner_id"}
 
-    nickname = fields.Char(required=True)
     partner_id = fields.Many2one(
         "res.partner", delegate=True, ondelete="cascade", required=True
     )
 
-    @api.constrains("nickname", "name")
-    def _check_nickname(self):
+    @api.constrains("name")
+    def _check_name(self):
         for friend in self:
-            if friend.nickname:
-                if not friend.nickname.isalpha():
-                    raise ValidationError(_("Nickname must be alphabetic"))
-
             if not friend.name.isalpha():
-                raise ValidationError(_("Name must be alphabetic"))
+                raise ValidationError(
+                    _(
+                        "Name must be alphabetic. "
+                        "Must not contain spaces or special characters or numbers."
+                    )
+                )
+
+    @api.constrains("email")
+    def _check_email(self):
+        for friend in self:
+            if friend.email and not self._is_valid_email(friend.email):
+                raise ValidationError(_("Invalid email address."))
+
+    def _is_valid_email(self, email):
+        email_regex = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        return re.match(email_regex, email) is not None
