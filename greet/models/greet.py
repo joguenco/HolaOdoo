f75b87e5fdc4b7fd76a8cc4546270436f67c75d3
Jorge Luis <jorjoluiso@gmail.com>
2024-12-21 10:22:41 -0500

    [REF] Greet model

--- greet/models/greet.py
+++ greet/models/greet.py
@@ -5,7 +5,6 @@ class Greet(models.Model):
     _name = "greet.greet"
     _description = "Greetings"
 
-    description = fields.Char()
     date = fields.Datetime(default=fields.Datetime.now)
-    partner_id = fields.Many2one("res.partner", "Partner", required=True)
+    reason = fields.Char()
     line_ids = fields.One2many("greet.greet.line", "greet_id", "Lines")
--- greet/models/res_company.py
+++ greet/models/res_company.py
@@ -1,18 +1,17 @@
-from odoo import models, api
+from odoo import models
+
 
 class ResCompany(models.Model):
-    _inherit = 'res.company'
-
+    _inherit = "res.company"
 
     def action_greet_me_ii(self):
         return {
-            'type': 'ir.actions.act_window',
-            'name': 'My Company',
-            'view_mode': 'form',
-            'view_id': self.env.ref('greet.view_me_form').id,
-            'res_model': 'res.company',
-            'res_id': self.env.company.id,
-            'target': 'current',
-            'context': {'create': False, 'delete': False, 'edit': False},
+            "type": "ir.actions.act_window",
+            "name": "My Company",
+            "view_mode": "form",
+            "view_id": self.env.ref("greet.view_me_form").id,
+            "res_model": "res.company",
+            "res_id": self.env.company.id,
+            "target": "current",
+            "context": {"create": False, "delete": False, "edit": False},
         }
-
