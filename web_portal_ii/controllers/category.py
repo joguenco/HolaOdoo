d7e0e92395ecd2e8523bf231d010b08e7d2155f9
Jorge Luis <jorjoluiso@gmail.com>
2024-12-26 21:27:28 -0500

    [IMP] Pagination

--- web_portal_ii/controllers/category.py
+++ web_portal_ii/controllers/category.py
@@ -1,28 +1,39 @@
-from odoo.http import request, route
+from odoo.http import request
+from odoo import fields
+from odoo import http
 
-from odoo.addons.website.controllers.main import Home
+from odoo.addons.portal.controllers.portal import CustomerPortal
 
-
-class Category(Home):
-    @route(
-        "/hola/odoo/v2/products/category/<model(product.category):category>",
+class Category(CustomerPortal):
+    @http.route(
+        [
+            "/hola/odoo/v2/products/category/<model(product.category):category>",
+            "/hola/odoo/v2/products/category/<model(product.category):category>/page/<int:page>",
+        ],
         auth="public",
         website=True,
     )
-    def products_category(self, category, **kwargs):
+    def products_category(self, category, page=1, **kwargs):
         category_name = f"Products of {category.name}"
 
-        products = (
-            request.env["product.template"]
-            .sudo()
-            .search(
-                [
-                    ("categ_id", "=", category.id),
-                    ("categ_id.is_published", "=", True),
-                    ("is_published", "=", True),
-                ]
-            )
+        domain = [
+            ("categ_id", "=", category.id),
+            ("categ_id.is_published", "=", True),
+            ("is_published", "=", True),
+        ]
+
+        products = request.env["product.template"].sudo().search(domain)
+        total = request.env["product.template"].sudo().search_count(domain)
+
+        pager = request.website.pager(
+            url='/hola/odoo/v2/products/category/<model(product.category):category>',
+            total=total,
+            page=page,
+            step=5,  # Number of items per page
         )
+
+        print(f"pager: {pager}")
+
 
         if not products:
             return request.render("web_portal_ii.404", status=404)
